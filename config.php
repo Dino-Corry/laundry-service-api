8fe0c92af22d50b8c4962de04c4f74739bc82b0f
Dino Corry <essen653@gmail.com>
2023-06-14 12:27:04 +0100

    working

--- config.php
+++ config.php
@@ -1,2 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+use MongoDB\Client;
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+
+function server($dbName) {
+    $uri = 'mongodb+srv://dino:Dino-boy123@cluster0.ddqzrq5.mongodb.net/';
+    
+    try {
+        // Create a new client and connect to the server
+        $client = new Client($uri);
+
+        // Select the database
+        $db = $client->selectDatabase($dbName);
+
+        return $db;
+    } catch (Exception $e) {
+        printf($e->getMessage());
+    }
+}
 ?>
